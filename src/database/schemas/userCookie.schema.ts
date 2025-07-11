e960e5fc7bbf9411267927ec9ae1027d32257815
kalush666 <jonathan.kalush@gmail.com>
2025-07-11 22:49:23 +0300

    userCookie schema

---
+++ src/database/schemas/userCookie.schema.ts
@@ -0,0 +1,16 @@
+import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
+import { UserResponse } from '../../../../common/Ro/user.ro';
+import { EXPERATION_TIME_NUMBER } from '../../../../common/constatns/cookies.constants';
+
+@Schema({
+  timestamps: true,
+  collection: 'userCookies',
+})
+export class UserCookie {
+  @Prop({ required: true, trim: true })
+  userDetailes: UserResponse;
+
+  @Prop({ required: true, trim: true, expires: EXPERATION_TIME_NUMBER })
+  cookie: string;
+}
+export const UserCookieSchema = SchemaFactory.createForClass(UserCookie);
